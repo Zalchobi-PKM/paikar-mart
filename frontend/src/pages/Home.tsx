e912e5a6cb65aed1ecacabc4c4c0ae5788469919
Siddik Munna <zalchobi@gmail.com>
2026-05-14 22:52:44 +0600

    STEP 2: Fix home layout - proper scroll handling for hero + grid

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,9 +3,16 @@ import QuickAccessGrid from "@/components/home/QuickAccessGrid";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-0">
-      <HeroSection />
-      <QuickAccessGrid />
+    <div className="h-[calc(100vh-128px)] flex flex-col overflow-hidden">
+      {/* Fixed hero section */}
+      <div className="flex-shrink-0">
+        <HeroSection />
+      </div>
+      
+      {/* Scrollable grid */}
+      <div className="flex-1 overflow-y-auto scrollbar-hide">
+        <QuickAccessGrid />
+      </div>
     </div>
   );
 }
